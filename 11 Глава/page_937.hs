a2d8452e4b5d560d816cd2327ff168ec5051878f
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2020-06-05 20:30:27 +0300

    page 937

--- 11 Глава/page_937.hs
+++ 11 Глава/page_937.hs
@@ -2,6 +2,6 @@ import Data.List
 import Data.Char
 
 main = do
-  -- l <- getLine
-  l' <- (intersperse '-' $ reverse $ map (\x -> toUpper x )) getLine
+  l <- getLine
+  let l' = intersperse '-' $ reverse $ map (\x -> toUpper x ) l
   putStrLn l'
